aef2eba9f609e4358dc10a36c4086621b62ac414
danielglickman <glickman1@mail.tau.ac.il>
2023-07-11 19:15:54 +0300

    load model each run

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from lib.data import get_loaders
 
 if os.path.isdir('/home/lab/glickmd1'):
     os.environ["HF_DATASETS_CACHE"] = "/home/lab/glickmd1/.cache/huggingface/datasets"
-    os.environ["CUDA_VISIBLE_DEVICES"] = str(utils.get_random_with_gpu_with_gb_free(30, 2))
+    os.environ["CUDA_VISIBLE_DEVICES"] = str(utils.get_random_with_gpu_with_gb_free(60, 1))
 # export HF_DATASETS_CACHE="/cortex/users/danielg/.cache/huggingface/datasets"
 # export TRANSFORMERS_CACHE="/cortex/users/danielg/.cache/huggingface/transformers"
 # os.environ["CUDA_VISIBLE_DEVICES"] = str(gpu)
@@ -39,34 +39,39 @@ def get_llm(model, cache_dir="llm_weights"):
 
 
 def main():
-    args, dataloader, device, model, tokenizer = setup()
+    args, dataloader, tokenizer = setup()
 
-    pruning_experiment(args, dataloader, device, model, tokenizer)
+    pruning_experiment(args, dataloader, tokenizer)
 
 
 def setup():
     args = get_args()
-    model_name = args.model.split("/")[-1]
-    print(f"loading llm model {args.model}")
-    model = get_llm(args.model, args.cache_dir)
-    model.eval()
     tokenizer = AutoTokenizer.from_pretrained(args.model, use_fast=False)
     print("loading calibdation data")
     dataloader, _ = get_loaders("c4", nsamples=args.nsamples, seed=args.seed, seqlen=2048, tokenizer=tokenizer)
     print("dataset loading complete")
+    return args, dataloader, tokenizer
+
+
+def get_device_and_model(args):
     device = torch.device("cuda:0")
+    print(f"loading llm model {args.model}")
+    model = get_llm(args.model, args.cache_dir)
+    model.eval()
     if "30b" in args.model or "65b" in args.model:  # for 30b and 65b we use device_map to load onto multiple A6000 GPUs, thus the processing here.
         device = model.hf_device_map["lm_head"]
     print("use device ", device)
-    return args, dataloader, device, model, tokenizer
+    return device, model
 
 
-def pruning_experiment(args, dataloader, device, model, tokenizer):
+def pruning_experiment(args, dataloader, tokenizer):
     if args.wandb_exp_name is not None and args.wandb_exp_name != "":
         import wandb
         wandb.init(project=args.wandb_exp_name, config=args)
         args = wandb.config
-        # wandb.config.update(args)
+
+    device, model = get_device_and_model(args)
+
     # Setting seeds for reproducibility
     np.random.seed(args.seed)
     torch.random.manual_seed(args.seed)
@@ -87,7 +92,7 @@ def pruning_experiment(args, dataloader, device, model, tokenizer):
             prune_activations(args, model, tokenizer, dataloader, device)
     ################################################################
     print("*" * 30)
-    sparsity_ratio = check_sparsity(model, args)
+    sparsity_ratio = check_sparsity(model, args) if not args.prune_method == "activations" else args.sparsity_ratio
     print(f"sparsity sanity check {sparsity_ratio:.4f}")
     print("*" * 30)
     ################################################################
